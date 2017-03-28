f9f005b1a065f8cd9d45547741040f1f3b15c55f
Collin Johnston <collin.j.johnston@gmail.com>
2017-03-28 15:56:31 -0400

    some changes

--- main/main.ino
+++ main/main.ino
@@ -9,18 +9,21 @@ int slideDown;
 int sweepSidePos;
 int wallDone;
 int stage;
+int sideVal;
+int backVal;
 int driveCor=5;//change this to modify how much the drive motors should be corrected when bot too close/too far from wall
 int wallTolerance=10;
 int leftMotorSpeed;
 int rightMotorSpeed;
+int pyramidSensed;//ping time where pyramid is infront of us
 
 const int slideMotorPin;
 const int sweepMotorPin;
-const int leftMotorPin;
-const int rightMotorPin;
+const int leftMotorPin=9;
+const int rightMotorPin=8;
 const int pyramidButton;
 
-float uSData[3];
+float uSData[4];
 int irData = 0;
 
 void setup() {
@@ -43,10 +46,12 @@ void loop() {
       sweepMotor.writeMicroseconds(1600);
       delay(100);//change this delay to increase/decrease the sweep angle
       sweepMotor.writeMicroseconds(1500);//sweep arm should be in left position now
-
-    case (2):
       leftMotorSpeed=1600;
       rightMotorSpeed=1600;
+      stage++;
+    case (2):
+      checkSensor(2);
+      checkSensor(0);
       drive_LeftMotor.writeMicroseconds(leftMotorSpeed);
       drive_RightMotor.writeMicroseconds(rightMotorSpeed);
       //check side ultrasonic sensor to see if we are close enough to the wall
@@ -55,12 +60,26 @@ void loop() {
         rightMotorSpeed+=driveCor;
         //too far from wall, need to correct path
       }
-      else if (usData[2]<sideVal-wallTolerance){
+      else if (uSData[2]<sideVal-wallTolerance){
         leftMotorSpeed+=driveCor;
         rightMotorSpeed-=driveCor;
+        //too close to wall, need to correct path
       }
       if (uSData[0] < wallDone) {
         //the bot has sweeped the whole wall, now we need to sweep the corner and then move on
+        drive_LeftMotor.writeMicroseconds(1600);
+        drive_RightMotor.writeMicroseconds(1450);
+        delay(500);//time we want to turn for
+        drive_LeftMotor.writeMicroseconds(1400);
+        drive_RightMotor.writeMicroseconds(1400);
+        do{
+        checkSensor(3);
+        }while(uSData[4]>backVal);//keep backing up until we are close enough to the wall behind us
+        drive_LeftMotor.writeMicroseconds(1500);
+        drive_RightMotor.writeMicroseconds(1500);//stop
+        sweepMotor.writeMicroseconds(1400);
+        delay(350);
+        sweepMotor.writeMicroseconds(1500);
         stage++;
       }
 
@@ -68,12 +87,12 @@ void loop() {
       if (uSData[1] < pyramidSensed) {
         //pyramid is sensed
         slideMotor.write(slideDown);//drop the slide
-        drive_LeftMotor.write();
-        drive_RightMotor.write();
+        drive_LeftMotor.writeMicroseconds(1400);
+        drive_RightMotor.writeMicroseconds(1400);
       }
   }
 }
-void checkSensor(const int uStoCheck) { //sensors are numbered 0-2 (0 is front, 1 is raised front, 2 is side)
+void checkSensor(const int uStoCheck) { //sensors are numbered 0-2 (0 is front, 1 is raised front, 2 is side, 3 is back)
   digitalWrite(uStoCheck, HIGH);
   delayMicroseconds(10);
   digitalWrite(uStoCheck, LOW);
